0c6290ebe45f2bb5b58ffc9d96f195bef80c009f
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-06-06 21:45:58 -0700

    Added a way to add chord labels

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -61,7 +61,7 @@ ctrls.controller('ComposerCtrl', ['$scope', '$state', '$window',
 			resetTabs();
 		}
 
-		//helper function for checking duplicates
+		// helper function for checking duplicates
 		function isDuplicate(tab){
 			// check to see if the list is empty
 			if($scope.chordList.length === 0){
@@ -100,8 +100,66 @@ ctrls.controller('ComposerCtrl', ['$scope', '$state', '$window',
 		};
 
 		// delete tabs
-		function deleteChord(chordID){
-			console.log(chordID);
+		$scope.deleteChord = function(chordID){
+			$scope.chordList.splice(chordID, 1);
+			resetTabs();
+		};
+
+		// create new draggable chord label
+		$scope.addLabel = function(chord){
+			// create the element and add the text;
+			var newLabel = document.createElement('div');
+			newLabel.className = 'draggable ' + chord.root + chord.mod + ' chord-label';
+			newLabel.innerHTML = chord.root + chord.mod;
+
+			// prepend new element to lyrics
+			lyrics = document.getElementById('lyrics');
+			lyrics.insertBefore(newLabel, lyrics.childNodes[0]);
 		}
+
+/***** copied interactjs code *****/
+		// target elements with the "draggable" class
+		interact('.draggable')
+		  .draggable({
+		    // enable inertial throwing
+		    inertia: true,
+		    // keep the element within the area of it's parent
+		    restrict: {
+		      restriction: "parent",
+		      endOnly: true,
+		      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+		    },
+		    // enable autoScroll
+		    autoScroll: true,
+
+		    // call this function on every dragmove event
+		    onmove: dragMoveListener,
+		    // call this function on every dragend event
+		    onend: function (event) {
+		      var textEl = event.target.querySelector('p');
+
+		      textEl && (textEl.textContent =
+		        'moved a distance of '
+		        + (Math.sqrt(event.dx * event.dx +
+		                     event.dy * event.dy)|0) + 'px');
+		    }
+		  }
+		);
+
+	  function dragMoveListener (event) {
+	    var target = event.target,
+	        // keep the dragged position in the data-x/data-y attributes
+	        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+	        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+	    // translate the element
+	    target.style.webkitTransform =
+	    target.style.transform =
+	      'translate(' + x + 'px, ' + y + 'px)';
+
+	    // update the posiion attributes
+	    target.setAttribute('data-x', x);
+	    target.setAttribute('data-y', y);
+	  }
 	}
 ]);
